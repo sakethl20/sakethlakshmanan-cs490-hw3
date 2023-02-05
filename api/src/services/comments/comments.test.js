997bdc7fec5f3054e5593087a2407735d16aa1a8
Saketh Lakshmanan <saketh_lakshmanan@yahoo.com>
2023-02-05 00:03:48 -0500

    Chapter 7 part 1 changes

--- api/src/services/comments/comments.test.js
+++ api/src/services/comments/comments.test.js
@@ -1,26 +1,28 @@
-import { comments, createComment } from './comments'
+import { AuthenticationError, ForbiddenError } from '@redwoodjs/graphql-server'
+
 import { db } from 'src/lib/db'
+
+import { comments, createComment, deleteComment } from './comments'
 
 describe('comments', () => {
   scenario(
     'returns all comments for a single post from the database',
-  async (scenario) => {
-    const result = await comments({ postId: scenario.comment.jane.postId })
-    const post = await db.post.findUnique({
-      where: { id: scenario.comment.jane.postId },
-      include: { comments: true },
-    })
-    expect(result.length).toEqual(post.comments.length)
-  })
+    async (scenario) => {
+      const result = await comments({ postId: scenario.comment.jane.postId })
+      const post = await db.post.findUnique({
+        where: { id: scenario.comment.jane.postId },
+        include: { comments: true },
+      })
+      expect(result.length).toEqual(post.comments.length)
+    }
+  )
 
   scenario('postOnly', 'creates a new comment', async (scenario) => {
     const comment = await createComment({
       input: {
         name: 'Billy Bob',
         body: 'What is your favorite tree bark?',
-        post: {
-          connect: { id: scenario.post.bark.id },
-        },
+        postId: scenario.post.bark.id,
       },
     })
 
@@ -29,4 +31,42 @@ describe('comments', () => {
     expect(comment.postId).toEqual(scenario.post.bark.id)
     expect(comment.createdAt).not.toEqual(null)
   })
+
+  scenario('allows a moderator to delete a comment', async (scenario) => {
+    mockCurrentUser({ roles: ['moderator'] })
+
+    const comment = await deleteComment({
+      id: scenario.comment.jane.id,
+    })
+    expect(comment.id).toEqual(scenario.comment.jane.id)
+
+    const result = await comments({ postId: scenario.comment.jane.id })
+    expect(result.length).toEqual(0)
+  })
+
+  scenario(
+    'does not allow a non-moderator to delete a comment',
+    async (scenario) => {
+      mockCurrentUser({ roles: 'user' })
+
+      expect(() =>
+        deleteComment({
+          id: scenario.comment.jane.id,
+        })
+      ).toThrow(ForbiddenError)
+    }
+  )
+
+  scenario(
+    'does not allow a logged out user to delete a comment',
+    async (scenario) => {
+      mockCurrentUser(null)
+
+      expect(() =>
+        deleteComment({
+          id: scenario.comment.jane.id,
+        })
+      ).toThrow(AuthenticationError)
+    }
+  )
 })
